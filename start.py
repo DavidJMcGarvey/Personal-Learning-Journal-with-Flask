bbacd14c1ab7279559209a34a900796c316de0bc
David McGarvey <davidjmcgarvey@gmail.com>
2019-03-23 15:05:40 -0700

    add template inheritance

--- start.py
+++ start.py
@@ -3,6 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    return render_template()
+def index(task="Python"):
+    return render_template('task.html', task=task)
+
+
+app.run(debug=True, port=8000, host='0.0.0.0')
