bea2b5a22d6bf81d6fa1890b3bb475e639574ef3
David McGarvey <davidjmcgarvey@gmail.com>
2019-03-31 10:18:03 -0700

    add User model

--- start.py
+++ start.py
@@ -1,12 +1,46 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, g, render_template
+from flask_login import LoginManager
+
+import users
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
 
 app = Flask(__name__)
+app.secret_key = 'cadgvnoearg4ks9s?8!dfn38andsvhw34efw4t9g4ujsdfgsjdgw'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
-@app.route('/')
-def index(task="Python"):
-    return render_template('index.html', task=task)
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return users.User.get(users.User.id == userid)
+    except users.DoesNotExist:
+        return None
 
 
-app.run(debug=True, port=8080, host='0.0.0.0')
+@app.before_request
+def before_request():
+    """Close the database connection after each request"""
+    g.db = users.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
+
+if __name__ == '__main__':
+    users.User.create_user(
+        username='Dave',
+        email='dave@email.com',
+        password='notpassword',
+        admin=True
+    )
+    app.run(debug=DEBUG, port=PORT, host=HOST)
--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 import datetime
 
+from flask_bcrypt import generate_password_hash
 from flask_login import UserMixin
 from peewee import *
 
@@ -16,3 +17,15 @@ class User(UserMixin, Model):
     class Meta:
         database = DATABASE
         order_by = ('-joined_at',)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
+        except IntegrityError:
+            raise ValueError("User already exists")
+
